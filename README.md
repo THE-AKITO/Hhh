27502d2ce562afb664de2f344efd526a6c695606
AKITO <dipeshkoli86@gmail.com>
2025-02-20 20:33:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# 🟢 Website Template 🟢
+
+# ⚜️ If you want to user the code :
+
+- Replace the hero video / logo / video preload image
+- Replace the wishlist button link
+- Replace the webpage name
+- Replace the slider images and text
+- Replace footer legal notice
+
+
+
+# 🧰 TODO 
+
+- [x] Cursor  
+- [ ] Better responsive  
+- [ ] Finishing the sliders  
+- [ ] Font changes  
+- [ ] Footer  
+- [ ] Header menu (Navbar)
+
+
+
+# Copyright 
+> we are not the owner of this pictures and videos this project is only a template for the use of swiper api and Html,CSS,Js
---
+++ src/app/main.js
@@ -0,0 +1,103 @@
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Initialize Main Swiper
+var mainSwiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Initialize Nested Swiper
+var nestedSwiper = new Swiper(".nestedSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".nested-swiper .swiper-pagination",
+    clickable: true,
+  },
+});
+
+function openVideo(url) {
+  const videoFrame = document.getElementById('video-frame');
+  videoFrame.src = url;
+  document.getElementById('video-modal').style.display = 'flex';
+}
+
+
+function closeVideo() {
+  document.getElementById('video-frame').src = '';
+  document.getElementById('video-modal').style.display = 'none';
+}
+
+
+
+
+const cursorSmall = document.querySelector('.small');
+
+const positionElement = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+let isMoving = false;
+
+const animate = (e) => {
+  if (!isMoving) {
+    isMoving = true;
+    requestAnimationFrame(() => {
+      positionElement(e);
+      isMoving = false;
+    });
+  }
+};
+
+window.addEventListener('mousemove', animate);
+
+// send mail
+
+
+function sendEmail() {
+  var email = document.getElementById('email').value;
+  if (email) {
+      var subject = encodeURIComponent("New Subscription");
+      var body = encodeURIComponent("New subscription from: " + email);
+      var mailtoLink = `mailto:armines765@gmail.com?subject=${subject}&body=${body}`;
+
+      window.location.href = mailtoLink;
+      return false; // Prevents the form from actually submitting
+  } else {
+      alert('Please enter a valid email address.');
+      return false; // Prevents the form from actually submitting
+  }
+}
+
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#preloader").style.visibility = "visible";
+        window.scrollTo(0, 0);
+  } else {
+      document.querySelector(
+        "#preloader").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
